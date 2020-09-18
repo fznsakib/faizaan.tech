4955ce9aab598a3d03f27931ab87c4d305a160c3
fznsakib <fznsakib@gmail.com>
2020-09-18 14:56:13 +0400

    Serving resume

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,5 @@
 const path = require('path')
 
-// *Slugs are now handled through Contentful*
-// Generate a slug for each post
-// module.exports.onCreateNode = ({ node, actions }) => {
-//   const { createNodeField } = actions
-
-//   if (node.internal.type == 'MarkdownRemark') {
-//     const slug = path.basename(node.fileAbsolutePath, '.md')
-
-//     createNodeField({
-//       node,
-//       name: 'slug',
-//       value: slug
-//     })
-//   }
-// }
-
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   
--- src/templates/post.js
+++ src/templates/post.js
@@ -4,17 +4,6 @@ import Head from "../components/head"
 import { graphql } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       frontmatter {
-//         title
-//         date
-//       }
-//       html
-//     }
-//   }
-// `
 
 export const query = graphql`
   query($slug: String!) {
@@ -39,15 +28,12 @@ export default function Blog(props) {
     }
   }
 
-  // const RenderPostHTML = (props) => (<div dangerouslySetInnerHTML={{ __html: props.html }}></div>)
-
   return (
     <Layout>
       <Head title={props.data.contentfulBlogPost.title}/>
       <h1>{ props.data.contentfulBlogPost.title }</h1>
       <p>{ props.data.contentfulBlogPost.publishedDate }</p>
       {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
-      {/* <RenderPostHTML html={props.data.contentfulBlogPost.html}/> */}
     </Layout>
   )
 }
