fd828029660f5f1390f43b3d91e05304e8a89011
fznsakib <fznsakib@gmail.com>
2020-09-18 17:19:10 +0400

    Header animations added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import Head from "../components/head";
 import ColourBlock from "../components/colourblock";
+import NameBlock from "../components/nameblock";
 import IconBar from "../components/iconbar";
 
 import indexStyles from "./index.module.scss";
@@ -11,12 +12,8 @@ export default function Home() {
     <div>
       <Layout>
         <Head title="Home" />
-        <div className={indexStyles.headers}>
-          <h1 className={indexStyles.name1}>faizaan sakib</h1>
-          <h1 className={indexStyles.name2}>faizaan sakib</h1>
-          <h1 className={indexStyles.name3}>faizaan sakib</h1>
-          <h1 className={indexStyles.name4}>faizaan sakib</h1>
-          <h1 className={indexStyles.jobTitle}>software engineer</h1>
+        <div className={indexStyles.blocks}>
+          <NameBlock></NameBlock>
           <ColourBlock></ColourBlock>
           <IconBar></IconBar>
         </div>
