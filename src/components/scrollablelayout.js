f026a53a0f0bc3faa35d87c4fce3629cd9458421
fznsakib <fznsakib@gmail.com>
2020-10-21 18:56:20 +0400

    Supporting overflowing content

---
+++ src/components/scrollablelayout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./navbar";
+
+import "../styles/index.scss";
+import scrollableLayoutStyles from "./scrollablelayout.module.scss";
+
+export default function Layout(props) {
+  return (
+    <div>
+      <div className={scrollableLayoutStyles.container}>
+        <div className={scrollableLayoutStyles.content}>{props.children}</div>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/layout";
+import ScrollableLayout from "../components/scrollablelayout";
 import Head from "../components/head";
 import Intro from "../components/intro";
 
@@ -8,12 +8,12 @@ import aboutStyles from "./about.module.scss"
 export default function About() {
   return (
     <div>
-      <Layout>
+      <ScrollableLayout>
         <Head title="About Me" />
         <Intro></Intro>
         <p className={aboutStyles.text}>I recently graduated from the University of Bristol with a Masters graduate in Computer Science (MEng), achieving First Class honours. Right now, I’m on the lookout for exciting opportunities in software engineering. Get in touch with me on LinkedIn!</p>
         <p className={aboutStyles.text}>During my time at university, along with software engineering internships, I have enjoyed working on many projects, aimed at solving all sorts of problems using different technologies. This has given me a strong breadth of skills, having had experience working in areas of computer vision, cloud computing, web development, VR, blockchain and deep learning, just to name a few. Through this, I have developed a keen attitude to constantly learn new technologies and be able to quickly adapt where necessary.</p>
-      </Layout>
+      </ScrollableLayout>
     </div>
   );
 }
