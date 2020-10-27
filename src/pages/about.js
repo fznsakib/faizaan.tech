c703eaa5777c5b2e69b3a4de4d14586df2499030
fznsakib <fznsakib@gmail.com>
2020-10-27 11:55:23 +0400

    Renamed grid to infogrid

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,7 @@ import Img from "gatsby-image"
 
 import ScrollableLayout from "../components/scrollablelayout";
 import Head from "../components/head";
-import Grid from "../components/grid"
-import LanguageSlider from "../components/slider"
+import InfoGrid from "../components/infogrid"
 
 import aboutStyles from "./about.module.scss"
 
@@ -28,13 +27,11 @@ export default function About() {
       <ScrollableLayout>
         <Head title="About Me" />
         <Img className={aboutStyles.profilePicture} fixed={data.file.childImageSharp.fixed} fadeIn="True" alt="Faizaan"/>
-
         <h1 className={aboutStyles.header}>about me</h1>
         <p className={aboutStyles.text}>I have recently graduated from the University of Bristol with a Masters in Computer Science (MEng), achieving first-class honours. Right now, I’m on the lookout for exciting opportunities in software engineering. Get in touch with me on LinkedIn!</p>
         <p className={aboutStyles.text}>I have enjoyed working on many projects, aimed at solving all sorts of problems using different technologies. This has given me a strong breadth of skills, with experience working in areas of cloud computing, web development, deep learning, VR, and blockchain, just to name a few. Through this, I have developed a keen attitude to constantly learn new technologies and be able to quickly adapt where necessary.</p>
         <h1 className={aboutStyles.header}>my skills</h1>
-        {/* <LanguageSlider></LanguageSlider> */}
-        <Grid></Grid>
+        <InfoGrid></InfoGrid>
       </ScrollableLayout>
     </div>
   );
