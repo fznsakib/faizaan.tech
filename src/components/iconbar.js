b60c33418a8e75ba21baaad80392c03a8703dab6
fznsakib <fznsakib@gmail.com>
2020-10-29 18:05:25 +0400

    Using outbound links for tracking

--- src/components/iconbar.js
+++ src/components/iconbar.js
@@ -7,6 +7,7 @@ import {
   faGithub,
   faSoundcloud,
 } from "@fortawesome/free-brands-svg-icons";
+import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 import iconBarStyles from "./iconbar.module.scss";
 
@@ -38,44 +39,44 @@ class IconBar extends Component {
           </a>
         </li>
         <li>
-          <a
+          <OutboundLink
             href="mailto:fznsakib@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
             className={iconBarStyles.emailIcon}
           >
             <FontAwesomeIcon icon={faEnvelope} size="2x" />
-          </a>
+          </OutboundLink>
         </li>
         <li>
-          <a
+          <OutboundLink
             href="https://www.linkedin.com/in/faizaan-sakib"
             target="_blank"
             rel="noopener noreferrer"
             className={iconBarStyles.linkedinIcon}
           >
             <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-          </a>
+          </OutboundLink>
         </li>
         <li>
-          <a
+          <OutboundLink
             href="https://www.github.com/fznsakib"
             target="_blank"
             rel="noopener noreferrer"
             className={iconBarStyles.githubIcon}
           >
             <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
+          </OutboundLink>
         </li>
         <li>
-          <a
+          <OutboundLink
             href="https://soundcloud.com/etaki"
             target="_blank"
             rel="noopener noreferrer"
             className={iconBarStyles.soundcloudIcon}
           >
             <FontAwesomeIcon icon={faSoundcloud} size="2x" />
-          </a>
+          </OutboundLink>
         </li>
       </ul>
     );
