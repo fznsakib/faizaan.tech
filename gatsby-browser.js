77db49a18bfb5cc210cf4ab0825cbad746c1c09b
fznsakib <fznsakib@gmail.com>
2021-02-13 13:22:27 +0400

    Initialised prism in gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
 import "./src/styles/index.scss";
+
+// Initialise prism with settings
+require('prismjs');
+require("prismjs/themes/prism-custom.css");
+require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+
+// Prism language requirements
+require('prismjs/components/prism-python');
+
+
