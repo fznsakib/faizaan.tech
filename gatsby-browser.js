22567b322b05a159f61eb58c7ece2290269da2d6
fznsakib <fznsakib@gmail.com>
2021-02-13 13:31:13 +0400

    Using prism.scss locally

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,8 @@
 import "./src/styles/index.scss";
+import "./src/styles/prism.scss";
 
-// Initialise prism with settings
 require('prismjs');
-require("prismjs/themes/prism-custom.css");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-
-// Prism language requirements
 require('prismjs/components/prism-python');
 
 
