f90af6a4581b358844e6f2b57b489f02b6dbcd05
fznsakib <fznsakib@gmail.com>
2021-04-02 17:51:09 +0400

    Add Cypher syntax to prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,7 @@
 import "./src/styles/index.scss";
 import "./src/styles/prism.scss";
 
-require('prismjs');
+require("prismjs");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-require('prismjs/components/prism-python');
-
-
+require("prismjs/components/prism-python");
+require("prismjs/components/prism-cypher");
