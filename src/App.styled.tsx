f6f9155c91fbe59e7c5a3ad3cdacb4027b953dde
Faizaan Sakib <faizaan.sakib@affirm.com>
2025-02-17 14:06:41 +0000

    feat: add head

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -1,13 +1,5 @@
-import styled, { keyframes } from "styled-components";
-
-const logoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
+import { Canvas } from "@react-three/fiber";
+import styled from "styled-components";
 
 export const AppContainer = styled.div`
   width: 100%;
@@ -20,39 +12,10 @@ export const AppContainer = styled.div`
   align-items: center;
 `;
 
-export const LogoContainer = styled.div``;
-
-export const Logo = styled.img`
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-
-  &:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-`;
-
-export const ReactLogo = styled(Logo)`
-  &:hover {
-    filter: drop-shadow(0 0 2em #61dafbaa);
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${logoSpin} infinite 20s linear;
-  }
-`;
-
-export const Title = styled.h1``;
-
-export const Card = styled.div`
-  padding: 2em;
-`;
-
-export const Button = styled.button`
-  margin-bottom: 1em;
-`;
-
-export const ReadTheDocs = styled.p`
-  color: #888;
+export const ThreeCanvas = styled(Canvas)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
